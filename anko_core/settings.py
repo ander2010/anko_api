00b4330ae5bf09f830713bd53874da17ebaabcc3
ander2010 <ernestocarlos2010@gmail.com>
2025-12-27 00:37:02 -0500

    anko-api.onrender.com accepted

--- anko_core/settings.py
+++ anko_core/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = 'django-insecure-!@@uw(j#in54g*gx_ky!yh7c9k4z1zu2d_z67)fhw@v)t$spxp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anko-api.onrender.com']
 
 
 # Application definition
