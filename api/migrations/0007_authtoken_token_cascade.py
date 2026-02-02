65688040dfcda1e181c82ff817928d35a43dabb4
RafaOsc <rafaeloscarginarteaguilar@gmail.com>
2026-02-01 23:46:07 -0500

    Create 0007_authtoken_token_cascade.py

---
+++ api/migrations/0007_authtoken_token_cascade.py
@@ -0,0 +1,51 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("api", "0006_emailverification_cascade"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+DO $$
+DECLARE
+  c RECORD;
+BEGIN
+  FOR c IN
+    SELECT conname
+    FROM pg_constraint
+    WHERE conrelid = 'authtoken_token'::regclass
+      AND contype = 'f'
+  LOOP
+    EXECUTE format('ALTER TABLE authtoken_token DROP CONSTRAINT %I', c.conname);
+  END LOOP;
+
+  ALTER TABLE authtoken_token
+    ADD CONSTRAINT authtoken_token_user_id_fk
+      FOREIGN KEY (user_id) REFERENCES api_user(id) ON DELETE CASCADE;
+END $$;
+""",
+            reverse_sql="""
+DO $$
+DECLARE
+  c RECORD;
+BEGIN
+  FOR c IN
+    SELECT conname
+    FROM pg_constraint
+    WHERE conrelid = 'authtoken_token'::regclass
+      AND contype = 'f'
+  LOOP
+    EXECUTE format('ALTER TABLE authtoken_token DROP CONSTRAINT %I', c.conname);
+  END LOOP;
+
+  ALTER TABLE authtoken_token
+    ADD CONSTRAINT authtoken_token_user_id_fk
+      FOREIGN KEY (user_id) REFERENCES api_user(id) ON DELETE NO ACTION;
+END $$;
+""",
+        ),
+    ]
