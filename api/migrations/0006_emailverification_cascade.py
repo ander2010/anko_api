85ce3a0998040e33f7708d5bf08aaf55ab4ca94a
RafaOsc <rafaeloscarginarteaguilar@gmail.com>
2026-02-01 23:43:50 -0500

    Update 0006_emailverification_cascade.py

--- api/migrations/0006_emailverification_cascade.py
+++ api/migrations/0006_emailverification_cascade.py
@@ -4,7 +4,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("api", "0005_alter_user_email"),
+        ("api", "0002_user_roles_cascade"),
     ]
 
     operations = [
