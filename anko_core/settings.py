c5d48ecf3d61ae848b432503cd9973b8cea80fe8
RafaOsc <rafaeloscarginarteaguilar@gmail.com>
2026-02-27 01:09:31 -0500

    Update settings.py

--- anko_core/settings.py
+++ anko_core/settings.py
@@ -245,7 +245,7 @@ CORS_ALLOW_CREDENTIALS = True
 # ===============================
 # CHANNELS (WebSocket)
 # ===============================
-REDIS_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379/0")
+REDIS_URL = os.getenv("REDIS_URL", "redis://hope-redis:6379/3")
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
